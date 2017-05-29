ef1e14cbf35105e14704f667e9c562495809be31
Joe Januszkiewicz <joe.januszk@gmail.com>
2017-05-29 16:00:25 -0400

    implemented PID controller with manually-tuned parameters

--- src/PID.cpp
+++ src/PID.cpp
@@ -2,20 +2,34 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  i_error = cte + i_error;
+  p_error = cte;
 }
 
 double PID::TotalError() {
+  double steer = -((Kp * p_error) + (Ki * i_error) + (Kd * d_error));
+  if (steer > 1) {
+    return 1;
+  }
+  if (steer < -1) {
+    return -1;
+  }
+  return steer;
 }
 
